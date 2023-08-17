020ea5b0ff60f8fe048dad20082c4966e38fc2b5
ev1lspirit <dkosmyninspb@mail.ru>
2023-08-17 14:03:37 +0400

    Core layer

---
+++ Notes.Domain/NotesDomain.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Notes.Domain
+{
+    public class Note
+    {
+        public Guid UserId { get; set; }
+        public Guid NoteId { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
---
+++ NotesApplication/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NotesApplication
+{
+    public class Class1
+    {
+    }
+}
